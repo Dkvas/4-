1669d8be643cbbea8e0b4d2f3ae448379778be47
Dmitry Kvasov <kvasof.d@gmail.com>
2022-10-11 17:54:30 +0300

    2 из 3х

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,3 +1,16 @@
 ﻿// Задача 25: Напишите метод, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B с использованием цикла.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+
+using static System.Console;
+Clear();
+Write("Введите число: ");
+int A = Convert.ToInt32(ReadLine());
+Write("Введите натуральную степень: ");
+int B = Convert.ToInt32(ReadLine());
+int Res=A;
+for(int i=1; i<B; i++) 
+    {
+        Res= Res*A;
+    }
+  WriteLine($"Число {A} в степени {B} равняется {Res}");
--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+using static System.Console;
+Clear();
+Write("Введите число: ");
+int num = Convert.ToInt32(ReadLine());
+int temp = num;
+int sum = 0;
+while (temp>0)
+{
+    sum = sum + temp%10;
+    temp = temp/10 ;
+}
+WriteLine($"Сумма цифр в числе {num} будет {sum}");
